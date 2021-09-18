8a7d40f0e2df621bed84df51ee7fc88c6e180091
sejal234 <sejalg101@gmail.com>
2021-09-18 14:07:56 -0500

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -3,8 +3,7 @@ let color = '#3aa757';
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
-}
-=======
+}),
 chrome.browserAction.onClicked.addListener(function(tab) {
 
 });
