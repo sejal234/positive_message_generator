c2927d8969522d82ebebeb41c897c600abd9c767
shannonshao <90943847+shannonshao@users.noreply.github.com>
2021-09-18 13:49:27 -0500

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,7 @@
-
 chrome.browserAction.onClicked.addListener(function(tab) {
 
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 
->>>>>>> acf246b0a26d4ea79065c4bc760337c3e6b5a4a0
 });
