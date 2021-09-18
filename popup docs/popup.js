4ac7c20d32353d2cc02adb19f7bdf8bdfee66429
sejal234 <sejalg101@gmail.com>
2021-09-18 14:28:50 -0500

    Update popup.js

--- popup docs/popup.js
+++ popup docs/popup.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
+    var checkPageButton = document.getElementById('clickIt');
+    checkPageButton.addEventListener('click', function()){
 
-});
+        chrome.tabs.getSelected(null, function(tab)){
+            alert("Hi what's up");
+
+        });
+    }, false);
+
+}, false);
