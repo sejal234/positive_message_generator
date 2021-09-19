bb27fd23559e74f856628729c3118898c1342882
sejal234 <sejalg101@gmail.com>
2021-09-18 19:48:48 -0500

    Update popup.js

--- popup_docs/popup.js
+++ popup_docs/popup.js
@@ -1,8 +1,17 @@
-let btn = document.createElement("button");
-btn.name(randomMess());
-document.body.appendChild(btn);
-btn.onclick = randomMess();
+//document.getElementById("changeColor")
 function randomMess(){
     var random_message = random["You got this!", "You're beautiful!", "You're amazing!", "Don't sweat the small stuff!"];
-    return random_message;
+    alert (random_message)
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    var changeFontButton = document.getElementById('changeColor');
+    changeFontButton.addEventListener('click', function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.executeScript(activeTab.id, {
+                function: "randomMess"
+            });
+        });
+    }, false);
+}, false);
