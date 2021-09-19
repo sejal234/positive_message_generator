c61c5fa95846dafc7c69ebb5f45567e8ed131de5
nram1 <90943799+nram1@users.noreply.github.com>
2021-09-18 20:19:24 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,4 +1,4 @@
 var random_message = new Array();
 random_message = ["you got this!", "you're amazing!", "you're beautiful!", "don't sweat the small stuff :)", "have a great day!", "today will be your day!", "YOU MADE THIS!!!!", "you have to go down to go up -sejal", "stop! it's hammer time", "we believe in you!", "good luck!!", "go you!", "you can do it :)", "you deserve a nap"];
-var num = Math.floor(Math.random() * random_message.length());    
+var num = Math.floor(Math.random() * random_message.length);    
 alert (random_message[num])
