a829bf2c2507ff79eb806ecc68bf41b2a1054b93
shannonshao <90943847+shannonshao@users.noreply.github.com>
2021-09-18 20:12:02 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,2 +1,4 @@
-var random_message = "You got this!"
-    alert (random_message)
+var random_message = new Array();
+random_message = ["you got this!", "you're amazing!", "you're beautiful"];
+var num = Math.floor((Math.random() * 3) + 1);    
+alert (random_message[num])
